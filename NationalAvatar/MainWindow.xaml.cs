0d4d98337c1eb000bf0cc6f7f03dd77a8bc25ed1
sealoyal <sealoyal@hotmail.com>
2023-09-25 15:26:53 +0800

    美化界面

--- NationalAvatar/MainWindow.xaml.cs
+++ NationalAvatar/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
+
+using HandyControl.Controls;
 
 using Microsoft.Win32;
 
@@ -15,7 +18,7 @@ namespace NationalAvatar;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : HandyControl.Controls.Window
 {
     private readonly List<AvatarTemplate> templates;
     private string avatarPath = string.Empty;
@@ -49,6 +52,7 @@ public partial class MainWindow : Window
         {
             avatarPath = dialog.FileName;
             await Refresh();
+            downloadBtn.IsEnabled = true;
         }
     }
 
@@ -94,6 +98,22 @@ public partial class MainWindow : Window
 
     private async void LiveImage_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
+        await SaveAvatar();
+    }
+
+    private async void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        await SaveAvatar();
+    }
+
+    private async Task SaveAvatar()
+    {
+        if (string.IsNullOrWhiteSpace(avatarPath))
+        {
+            Growl.Error("请先上传图像");
+            return;
+        }
+
         SaveFileDialog dialog = new SaveFileDialog();
         dialog.Filter = "图片文件(*.png)|*.png";
         var r = dialog.ShowDialog() ?? false;
@@ -106,10 +126,12 @@ public partial class MainWindow : Window
         var s = Application.GetResourceStream(uri);
         var temp = await Image.LoadAsync(s.Stream);
         temp.Mutate(ctx => ctx.Resize(sourceImage.Size));
-        sourceImage.Mutate(async ctx =>
-        {
-            ctx.DrawImage(temp, 1f);
-        });
+        sourceImage.Mutate(async ctx => ctx.DrawImage(temp, 1f));
         await sourceImage.SaveAsPngAsync(fileName);
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start("github.com/");
     }
 }
